0079a0bcfe2de50af3978e68e44cb97204727e84
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-23 00:35:20 +0300

    JDBC

--- src/test/java/jdbctests/TestConnection.java
+++ src/test/java/jdbctests/TestConnection.java
@@ -1,9 +1,21 @@
 package jdbctests;
 
+import java.sql.*;
+
 public class TestConnection {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String dbUrl="jdbc:oracle:thin:@34.227.92.52:1521:XE";
         String dbUsername="hr";
         String dbPassword="hr";
+
+        Connection connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet=statement.executeQuery("SELECT * FROM employees");
+
+
+        // close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 }
