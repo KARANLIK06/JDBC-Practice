7cce632a7c6f4f505898c17a306cb8aa4f909d7c
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-23 00:27:05 +0300

    JDBC

--- src/test/java/jdbctests/TestConnection.java
+++ src/test/java/jdbctests/TestConnection.java
@@ -2,6 +2,8 @@ package jdbctests;
 
 public class TestConnection {
     public static void main(String[] args) {
-
+        String dbUrl="jdbc:oracle:thin:@34.227.92.52:1521:XE";
+        String dbUsername="hr";
+        String dbPassword="hr";
     }
 }
